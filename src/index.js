44d670ed95e4ea779531435fcb8a831e68b76ac8
unknown <pdullabh315@gmail.com>
2020-12-24 16:44:00 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import thunkMiddleware from 'redux-thunk';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {searchRobots, requestRobots} from './reducers';
+import {createStore, applyMiddleware, combineReducers} from 'redux';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons'; 
+import {createLogger} from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const logger = createLogger();
+const rootReducer = combineReducers({searchRobots, requestRobots})
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger))
+
+ReactDOM.render(<Provider store = {store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
